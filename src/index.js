4abc346ea132fa55dfeb6c0a67c080f4d8305021
varunjain999 <vvarunjain999@gmail.com>
2026-09-17 15:51:09 +0530

    utils code, apiErrors ,asyncHandler etc

--- src/index.js
+++ src/index.js
@@ -5,7 +5,19 @@ dotenv.config({
     path: './env'
 })
 
-connectDB();
+connectDB()
+    .then(()=>{
+        app.on("error",(error)=>{
+            console.log("ERROR : ", error)
+            throw error
+        })
+        app.listen(process.env.PORT || 8000,()=>{
+            console.log(`server is running at ${process.env.PORT}`)
+        })
+    })
+    .catch((err)=>{
+        console.log("Mongo db connection Failed",err)
+    })
 
 
 /*
